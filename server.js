c7b70a52b861bfa6814b6f549c6a6a6b7104685b
arunvictor007 <31340588+arunvictor007@users.noreply.github.com>
2017-08-25 20:33:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/page-one', function(req,res)
+{
+    res.send('<h1>This is page one.<h1>');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
