4b95f11dd9a008bb9bff8cb771a60890c6233861
arunvictor007 <31340588+arunvictor007@users.noreply.github.com>
2017-08-25 23:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 var pages={
-    pageOne:{
+    'page-one':{
     title:'Welcome to Page One',
     heading:'Page One(',
     date:'August 25, 2017',
@@ -14,14 +14,14 @@ var pages={
             <br>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>`
 },
-    pageTwo:{
+    'page-two':{
         title:'Page Two',
     heading:'Page Two',
     date:'August 26, 2017',
     content:`<p>This is the second Page</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             `
     },
-    pageThree:{title:'Page Three',
+    'page-three':{title:'Page Three',
     heading:'Page Three',
     date:'August 27, 2017',
     content:`<p>Third Page</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>`}
