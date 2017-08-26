3c0e7ad64f99752bab5e975f6c6fe952e32e0389
arunvictor007 <31340588+arunvictor007@users.noreply.github.com>
2017-08-26 10:40:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var image=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     image.style.marginLeft=marginLeft+'px';
 }
 image.onclick= function(){
-  var interval=setInterval(moveRight,50);  
+  var interval=setInterval(moveRight,10);  
     
 };
